4ca748125485bc3f3505e0bc1e43c067d56f54d0
Chao Meng <mengchao.agile@gmail.com>
2015-02-24 04:39:02 +0000

    Update run script for Programming Assignment - Membership Protocol

--- mp1_assignment/run.sh
+++ mp1_assignment/run.sh
@@ -7,13 +7,15 @@
 #* 
 #***********************
 #!/bin/sh
-sudo mkdir grade-dir
+mkdir grade-dir
 cd grade-dir
-sudo wget https://spark-public.s3.amazonaws.com/cloudcomputing/assignments/mp1/mp1.zip || { echo 'ERROR ... Please install wget'; exit 1; }
-sudo unzip mp1.zip || { echo 'ERROR ... Zip file not found' ; exit 1; }
-sudo cp ../MP1Node.* .
+wget https://spark-public.s3.amazonaws.com/cloudcomputing/assignments/mp1/mp1.zip || { echo 'ERROR ... Please install wget'; exit 1; }
+unzip mp1.zip || { echo 'ERROR ... Zip file not found' ; exit 1; }
+cd mp1
+cp ../../MP1Node.* .
 make clean > /dev/null
 make > /dev/null
+
 case $1 in
 	0) echo "Single failure"
 	./Application testcases/singlefailure.conf > /dev/null;;
@@ -23,6 +25,7 @@ case $1 in
 	./Application testcases/msgdropsinglefailure.conf > /dev/null;;
 	*) echo "Please enter a valid option";;
 esac
-cp dbg.log ../
-cd ..
-sudo rm -rf grade-dir
+
+cp dbg.log ../../
+cd ../../
+rm -rf grade-dir
